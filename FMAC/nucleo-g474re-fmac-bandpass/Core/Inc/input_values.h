fe8cb571ac149687620083fbf99d650630489dc6
Yevgen Melnyk <evgenmlnk@gmail.com>
2024-07-07 22:08:54 +0200

    FMAC bandpass filter order changed to 100

---
+++ FMAC/nucleo-g474re-fmac-bandpass/Core/Inc/input_values.h
@@ -0,0 +1,15 @@
+/*
+ * input_values.h
+ *
+ *  Created on: Jul 7, 2024
+ *      Author: evgen
+ */
+
+#ifndef INC_INPUT_VALUES_H_
+#define INC_INPUT_VALUES_H_
+
+extern const float aSineWave_y[2001];
+extern const float aInputValues_yn[2001];
+
+
+#endif /* INC_INPUT_VALUES_H_ */
